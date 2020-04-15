1affdec03295ce49dc42f8f91a5ef4e16c2f94b0
redhelmet8 <davethurlo2@gmail.com>
2020-04-15 10:20:12 -0600

    reds a dipshit

--- src/main/java/net/skyprison/Main/Commands/RanksPkg/GuardDuty.java
+++ src/main/java/net/skyprison/Main/Commands/RanksPkg/GuardDuty.java
@@ -5,9 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffectType;
 
 public class GuardDuty implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
