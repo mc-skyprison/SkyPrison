e76472a7e9c9aba1b883a4ed1cc04bbee9554a18
DrakePork <drakepork@hotmail.com>
2022-09-22 13:29:05 +0200

    Changed command name from sptags to tags

--- src/main/java/net/skyprison/skyprisoncore/SkyPrisonCore.java
+++ src/main/java/net/skyprison/skyprisoncore/SkyPrisonCore.java
@@ -322,7 +322,7 @@ public class SkyPrisonCore extends JavaPlugin {
         Objects.requireNonNull(getCommand("s")).setExecutor(new Staff(new ChatUtils(this, discApi)));
         Objects.requireNonNull(getCommand("donorreset")).setExecutor(new DonorReset(this));
         Objects.requireNonNull(getCommand("customenchant")).setExecutor(new CustomEnchant(this));
-        Objects.requireNonNull(getCommand("sptags")).setExecutor(new Tags(this, getDatabase()));
+        Objects.requireNonNull(getCommand("tags")).setExecutor(new Tags(this, getDatabase()));
     }
 
 
